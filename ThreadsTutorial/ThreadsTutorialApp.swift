565a7a331ad133ee62fcf3a52867a25052d32ab6
Cesario Nivar <ing.cesarionivar@gmail.com>
2025-06-12 20:17:33 -0400

    Initial Commit

---
+++ ThreadsTutorial/ThreadsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialApp.swift
+//  ThreadsTutorial
+//
+//  Created by Cesario Nivar on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
