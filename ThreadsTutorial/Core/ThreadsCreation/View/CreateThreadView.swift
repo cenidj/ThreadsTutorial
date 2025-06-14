888ff810fa25dfdbd5ac38adcc3d5adfa6da2cd6
Cesario Nivar <ing.cesarionivar@gmail.com>
2025-06-14 00:23:09 -0400

    :construction: Creating an user

--- ThreadsTutorial/Core/ThreadsCreation/View/CreateThreadView.swift
+++ ThreadsTutorial/Core/ThreadsCreation/View/CreateThreadView.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct CreateThreadView: View {
+    @State private var caption = ""
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text("Upload Thread")
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top) {
+                    CircularProfileImageView()
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("maxverstappen1")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Start a thread...", text: $caption)
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if !caption.isEmpty {
+                        Button {
+                            caption = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                    
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Post") {
+                        
+                    }
+                    .opacity(caption.isEmpty ? 0.5 : 1.09)
+                    .disabled(caption.isEmpty ? true : false)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+                }
+            }
+        }
     }
 }
 
